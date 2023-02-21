d97eb0242cbeda038dcf3c634a56100c8ed054ab
Vadym <besarabvadim2@gmail.com>
2023-02-21 20:46:30 +0200

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -6,9 +6,4 @@
     require "PHPMailer/src/Exception.php";
     require "PHPMailer/src/PHPMailer.php";
 
-    $mail = new PHPMailer(true); /* Создаем объект MAIL */
-$mail->CharSet = "UTF-8"; /* Задаем кодировку UTF-8 */
-$mail->IsHTML(true); /* Разрешаем работу с HTML */
-
-$name = $_POST["name"]; /* Принимаем имя пользователя с формы .. */
-$phone = $_POST["phone"]; /* Телефон */
+    
